e7776935d6e864c9b8e14154164ec842265e99ae
sude açıl <sudeacil80@gmail.com>
2021-02-08 21:21:23 +0300

    23 ocak ders sonu

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        //Method injection
+        public void BasvuruYap(IKrediManager krediManager, ILoggerService loggerService)
+        {
+            //Basvuran bilgilerini degerlendirme
+            //
+            krediManager.Hesapla();
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
---
+++ SQLQuery2.sql
@@ -0,0 +1,32 @@
+﻿--Select
+--ANSII
+Select ContactName Adi,CompanyName SirketAdi,City Sehir from Customers
+
+Select * from Customers where City = 'Berlin'
+
+--case insensitive buyuk kucuk harf duyarsiz
+seLECt * from Products where categoryId=1 or categoryId = 3
+
+seLECt * from Products where categoryId=1 and UnitPrice>=10
+
+select * from Products where categoryId=1 order by UnitPrice desc    --ascending artan --descending azalan
+ 
+ Select count(*) Adet from Products where CategoryId =2
+
+ Select categoryID,count(*) from products where unitPrice>20
+ group by CategoryID having count(*)<10
+
+ select Products.ProductID, Products.ProdutcName, Products.UnitPrice, Categories.CategoryName 
+ from products inner join Categories 
+ on Products.CategoryID = Categories.CategoryID
+ where Products.UnitPrice>10
+ 
+ --DTO Data Transformation object
+ --p = products, order dtails= od kisaltma
+
+ Select * from Products p left join [Order Details] od 
+ on p.ProductID = od.ProductID
+
+ Select * from Customers c left join Orders o
+ on c.CustomerID = o.CustomerID
+
